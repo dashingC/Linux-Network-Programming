ecb595a74b344c8d4a66fb0056a26490eb20d27f
dashingC <qincy.99@gmail.com>
2025-06-05 20:31:21 +0800

    select 头文件

--- lession/lession62/selectServer.hpp
+++ lession/lession62/selectServer.hpp
@@ -7,20 +7,51 @@
 namespace select_ns
 {
     static const int defaultport = 8080;
+    static const int defaultfd = -1;
     class SlectServer
     {
     public:
-        SlectServer(int port = defaultport) : _port(port), _listensock(-1)
+        SlectServer(int port = defaultport) : _port(port), _listensock(-1), fdarray(nullptr)
         {
+        }
+
+        void Print()
+        {
+            for (int i = 0; i < FD_SETSIZE; i++)
+            {
+                if (fdarray[i]!= defaultfd)
+                std::cout << "fdarray[" << i << "] = " << fdarray[i] << std::endl;
+            }
         }
         void HanderEvent(fd_set &rfds)
         {
-            //listensocket
+            // listensocket
             std::string clientip;
-            uint16_t clientport=0;
+            uint16_t clientport = 0;
             int sock = Sock::Accept(_listensock, &clientip, &clientport);
-            if (sock < 0) return;
+            if (sock < 0)
+                return;
             logMessage(NORMAL, "accept success[%s:%d]", clientip.c_str(), clientport);
+            // 将新连接的套接字添加到文件描述符数组中
+            int i = 0;
+            for (; i < FD_SETSIZE; i++)
+            {
+                if (fdarray[i] != defaultfd)
+                    continue; // 跳过已使用的文件描述符
+                else
+                    break;
+            }
+            if (i == FD_SETSIZE)
+            {
+                logMessage(WARNING, "fdarray is full, cannot accept new connection");
+                close(sock); // 关闭新连接
+            }
+            else
+            {
+                fdarray[i] = sock; // 将新连接的套接字添加到数组中
+                // logMessage(NORMAL, "add new fd[%d] to fdarray", sock);
+            }
+            Print();
         }
 
         void initServer()
@@ -28,6 +59,12 @@ namespace select_ns
             _listensock = Sock::Socket();
             Sock::Bind(_listensock, _port);
             Sock::Listen(_listensock);
+            fdarray = new int[FD_SETSIZE];
+            for (int i = 0; i < FD_SETSIZE; i++)
+            {
+                fdarray[i] = defaultfd; // 初始化文件描述符数组
+            }
+            fdarray[0] = _listensock; // 将监听套接字放在数组的第一个位置
             logMessage(NORMAL, "Server is listening");
         }
 
@@ -35,12 +72,21 @@ namespace select_ns
         {
             fd_set rfds;
             FD_ZERO(&rfds);
-            FD_SET(_listensock, &rfds); // 将监听套接字添加到读文件描述集中
+            int maxfd = _listensock; // 最大文件描述符
+            for (int i = 0; i < FD_SETSIZE; i++)
+            {
+                if (fdarray[i] == defaultfd)
+                    continue;              // 跳过未使用的文件描述符
+                FD_SET(fdarray[i], &rfds); // 将合法fd添加到读文件描述集中
+                if (fdarray[i] > maxfd)
+                    maxfd = fdarray[i]; // 更新最大文件描述符
+            }
+
             while (true)
             {
                 struct timeval timeout = {1, 0}; // 设置超时时间为3秒0微秒
 
-                int n = select(_listensock + 1, &rfds, nullptr, nullptr, nullptr);
+                int n = select(maxfd + 1, &rfds, nullptr, nullptr, nullptr);
                 switch (n)
                 {
                 case -1:
@@ -53,19 +99,6 @@ namespace select_ns
                 default:
                     logMessage(NORMAL, "get a new link ");
                     HanderEvent(rfds);
-                    // if (FD_ISSET(_listensock, &rfds)) {
-                    //     // 有新连接到来
-                    //     std::string clientip;
-                    //     uint16_t clientport;
-                    //     int sock = Sock::Accept(_listensock, &clientip, &clientport);
-                    //     if (sock < 0) {
-                    //         std::cerr << "Accept error" << std::endl;
-                    //         continue;
-                    //     }
-                    //     std::cout << "New connection from " << clientip << ":" << clientport << std::endl;
-                    //     // 处理新连接
-                    //     // ...
-                    // }
                     break;
                 }
                 // sleep(1); // 模拟处理其他任务
@@ -79,10 +112,13 @@ namespace select_ns
         {
             if (_listensock < 0)
                 close(_listensock);
+            if (fdarray)
+                delete[] fdarray;
         }
 
     private:
         int _port;
         int _listensock;
+        int *fdarray;
     };
 }
